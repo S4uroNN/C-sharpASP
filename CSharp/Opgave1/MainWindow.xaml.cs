8338b5f5a8ee7a08b35a78b050f321f79a7c22f3
S4uroNN <alin_constantin15@hotmail.com>
2023-02-20 19:39:16 +0100

    Lektion6_WPF

---
+++ CSharp/Opgave1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Opgave1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btn1.Visibility = Visibility.Hidden;
+        }
+
+        private void CheckBox_Onclick(object sender, RoutedEventArgs e)
+        {
+            if(checkBox1.IsChecked == true)
+            {
+                btn1.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                btn1.Visibility = Visibility.Hidden;
+            }
+
+            if(checkBox1.IsChecked == false)
+            {
+                btn1.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                btn1.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            btn1.Content = "RED BUTTON!";
+        }
+    }
+}
