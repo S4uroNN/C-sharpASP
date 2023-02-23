7412ebebc7a9a031b98f8ac66d9e54e6eff7b844
S4uroNN <alin_constantin15@hotmail.com>
2023-02-23 08:32:10 +0100

    lektion7

---
+++ CSharp/Opgave7_1/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Opgave7_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Person person;
+        private ObservableCollection<Person> persons = new ObservableCollection<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            person = new Person("Lasse", 70, 23, 69420, true);
+
+           
+
+            persons.Add(person);
+            persons.Add(new Person("Kristina", 56, 23, 55, false));
+
+            
+            listConsol.ItemsSource = persons;
+            mainGrid.DataContext = persons;
+
+
+
+        }
+
+        private void txtName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (listConsol.SelectedItem != null)
+            {
+               ((Person)listConsol.SelectedItem).Name =((TextBox)sender).Text;
+            }
+        }
+        private void txtAge_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (listConsol.SelectedItem != null)
+            {
+                ((Person)listConsol.SelectedItem).Age = Int32.Parse(((TextBox)sender).Text);
+            }
+        }
+        private void txtWeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (listConsol.SelectedItem != null)
+            {
+                ((Person)listConsol.SelectedItem).Weight = Int32.Parse(((TextBox)sender).Text);
+            }
+        }
+        private void txtScore_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (listConsol.SelectedItem != null)
+            {
+                ((Person)listConsol.SelectedItem).Score = Int32.Parse(((TextBox)sender).Text);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            person.Name = "Morten";
+            person.Age = 23;
+            person.Weight = 59;
+            person.Score = 1337;
+        }
+
+        private void btnTilføj_Click(object sender, RoutedEventArgs e)
+        {
+            String name = txtName.Text;
+            int age = Int32.Parse(txtAge.Text);
+            int weight = Int32.Parse(txtWeight.Text);
+            int score = Int32.Parse(txtScore.Text);
+            Boolean accepted = chkAcc.IsChecked  == true;
+
+            persons.Add(new Person(name, weight, age, score, accepted));
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            listConsol.ItemsSource = null;
+            listConsol.ItemsSource = persons;
+        }
+    }
+}
---
+++ CSharp/Opgave7_1/Person.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave7_1
+{
+    internal class Person : INotifyPropertyChanged
+    {
+        private string _name;
+        private int _weight;
+        private int _age;
+        private int _score;
+        private Boolean _accepted;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+        public int Weight
+        {
+            get
+            {
+                return _weight;
+            }
+
+            set
+            {
+                _weight = value;
+                NotifyPropertyChanged("Weight");
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            set
+            {
+                _age = value;
+                NotifyPropertyChanged("Age");
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                return _score;
+            }
+            set
+            {
+                _score = value;
+                NotifyPropertyChanged("Score");
+            }
+        }
+
+        public Boolean Accepted 
+        {
+            get
+            {
+                return _accepted;
+            }
+
+            set
+            {
+                _accepted = value;
+                NotifyPropertyChanged("Accepted");
+            }
+        }
+
+        public Person(String name, int weight, int age, int score, Boolean accepted)
+        {
+            Name = name;
+            Weight = weight;
+            Age = age;
+            Score = score;
+            Accepted = accepted;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Navn: " +Name +  " Alder: " + Age + " Score: " + Score;
+        }
+    }
+}
