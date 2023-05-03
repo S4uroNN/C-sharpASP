8bb0a3a38b073923aace10fb06416f7a55f92d03
S4uroNN <alin_constantin15@hotmail.com>
2023-05-03 08:43:48 +0200

    Old stuff

---
+++ CSharp/Lektion15/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using Lektion15.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lektion15.Controllers
+{
+    public class PersonController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            Person p = new Person("Ole","Hansen",95);
+           
+            return View("index",p);
+        }
+
+        
+        public IActionResult Find(string firstname, string lastname,int id)
+        {
+            return View();
+        }
+    }
+}
